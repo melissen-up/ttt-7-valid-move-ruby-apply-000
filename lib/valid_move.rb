4b3becf0452b274427c55d4acdafd5baec190b05
Melissa F <melissa.faulner@gmail.com>
2020-10-06 02:09:39 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && if position_taken?(board, index) == FALSE
+  if index.between?(0, 8) 
+    position_taken?(board, index)
     return TRUE
   end
 end
